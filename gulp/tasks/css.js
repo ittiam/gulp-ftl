2e880000c2908b816ea05ddc31a096ddfdf5ed6f
czh <ittiamc@gmail.com>
2016-06-28 16:57:10 +0800

    task css

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -5,7 +5,7 @@ const path = require('path');
 const gulp = require('gulp');
 const gulpif = require('gulp-if');
 const changed = require('gulp-changed');
-const handleErrors = require('../lib/handleErrors');
+const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const minifyCSS = require('gulp-cssnano');
 const browserSync = require('browser-sync');
@@ -14,17 +14,19 @@ const cssTask = function () {
   const dest = global.production ? config.root.dest : config.root.tmp;
 
   const paths = {
-    src: path.join(config.root.src, config.tasks.css.src, '/**/*.{' + config.tasks.css.extensions + '}'),
+    src: path.join(config.root.src, config.tasks.css.src, '/**/*.css'),
     dest: path.join(dest, config.tasks.css.dest)
   };
 
   return gulp.src(paths.src)
     .pipe(changed(paths.dest))
+    .pipe(gulpif(!global.production, sourcemaps.init()))
     .pipe(autoprefixer(config.tasks.css.autoprefixer))
     .pipe(gulpif(global.production, minifyCSS({autoprefixer: false})))
+    .pipe(gulpif(!global.production, sourcemaps.write('.')))
     .pipe(gulp.dest(paths.dest))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
 }
 
-gulp.task('css', cssTask)
-module.exports = cssTask
+gulp.task('css', cssTask);
+module.exports = cssTask;
